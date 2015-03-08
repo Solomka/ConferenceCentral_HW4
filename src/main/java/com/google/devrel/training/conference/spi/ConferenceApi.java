7399ffc53e6d22440a68ea4caa7c5a82a9181cc5
Solomka <sol.yaremko@gmail.com>
2015-03-08 14:29:52 +0200

    Just to safe version

--- src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -257,36 +257,34 @@ public class ConferenceApi {
     }
 
 
-    public List<Conference> filterPlayground() {
-        // Query<Conference> query = ofy().load().type(Conference.class).order("name");
-        Query<Conference> query = ofy().load().type(Conference.class);
+    
+    	public List<Conference> filterPlayground() {
+            Query<Conference> query = ofy().load().type(Conference.class).order("name");
 
-     
-        // Filter on city
-        query = query.filter("city =", "London");
-        // query = query.filter("city =", "Default City");
-        /*
-        // Add a filter for topic = "Medical Innovations"
-        query = query.filter("topics =", "Medical Innovations");
+            // Filter on city
+            query = query.filter("city =", "London");
 
-        // Add a filter for maxAttendees
-        query = query.filter("maxAttendees >", 8);
-        query = query.filter("maxAttendees <", 10).order("maxAttendees").order("name");
+            /*
+            // Add a filter for topic = "Medical Innovations"
+            query = query.filter("topics =", "Medical Innovations");
 
-        // Add a filter for month {unindexed composite query}
-        // Find conferences in June
-        query = query.filter("month =", 6);
-        */
+            // Add a filter for maxAttendees
+            query = query.filter("maxAttendees >", 8);
+            query = query.filter("maxAttendees <", 10).order("maxAttendees").order("name");
 
-        // multiple sort orders
+            // Add a filter for month {unindexed composite query}
+            // Find conferences in June
+            query = query.filter("month =", 6);
 
-      /*  query = query.filter("city =", "Tokyo").filter("seatsAvailable <", 10).
-                filter("seatsAvailable >" , 0).order("seatsAvailable").order("name").
-                order("month");
-*/
+            // multiple sort orders
+            query = query.filter("city =", "Tokyo").filter("seatsAvailable <", 10).
+                    filter("seatsAvailable >" , 0).order("seatsAvailable").order("name").
+                    order("month");
+            */
 
-        return query.list();
-    }
+            return query.list();
+        }
+    
 
 
     /**
